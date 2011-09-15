c6b53f142082a41092639e3f4918e20ff37662f3
Ilkka Prusi <ilkka.prusi@gmail.com>
2011-09-15 21:27:18 +0300

    additional handling, starting to debug..

--- AhxPlayer.cpp
+++ AhxPlayer.cpp
@@ -6,6 +6,10 @@
 //
 // Based on documentation by: Stuart Caie
 //
+// Note on implementation:
+// could use bitfield-structs in some data
+// but struct packing might be a problem with some compilers
+// so I'd rather not do that and done in harder way..
 //
 // Author: Ilkka Prusi, 2011
 // Contact: ilkka.prusi@gmail.com
@@ -17,6 +21,7 @@
 // for buffer-wrapper..
 #include "AnsiFile.h"
 
+
 // use buffer only, can switch implementation easily then..
 CAhxPlayer::CAhxPlayer(CReadBuffer *pFileData)
     : CModPlayer(pFileData)
@@ -26,6 +31,7 @@ CAhxPlayer::CAhxPlayer(CReadBuffer *pFileData)
     , m_trackListData(nullptr)
     , m_trackZero(nullptr)
     , m_sampleData(nullptr)
+    , m_sampleNames(nullptr)
 {
     // needed anyway?
     m_trackZero = new uint8_t[192];
@@ -34,6 +40,7 @@ CAhxPlayer::CAhxPlayer(CReadBuffer *pFileData)
 
 CAhxPlayer::~CAhxPlayer()
 {
+    delete [] m_sampleNames;
     delete [] m_sampleData;
     delete m_trackZero;
     delete [] m_trackListData;
@@ -69,7 +76,10 @@ bool CAhxPlayer::ParseFileInfo()
     
     // some flags
     uint8_t temp = ReadUI8();
-    m_trackSave = (temp & 0xF0);
+    
+    // if track zero is saved in file or not
+    // note: must be 1/0 for later..
+    m_trackZeroSaved = ((temp & 0xF0) ? 1 : 0);
     m_playSpeed = (temp & 0x0F);
     
     m_posListLen = (ReadBEUI16() & 0xFF); // LEN
@@ -83,7 +93,7 @@ bool CAhxPlayer::ParseFileInfo()
     // subsong list
     if (m_subsongCount > 0)
     {
-        m_subsongList = AHXSubsongEntry_t[0];
+        m_subsongList = new AHXSubsongEntry_t[m_subsongCount];
         for (int i = 0; i < m_subsongCount; i++)
         {
             // just a value, nothing more to it?
@@ -92,19 +102,23 @@ bool CAhxPlayer::ParseFileInfo()
     }
     
     // position list
-    m_positionList = new AHXPositionEntry_t[m_posListLen];
-    for (int i = 0; i < m_posListLen; i++)
+    if (m_posListLen > 0)
     {
-        // 8 bytes, 4 sets (for each audiochannel), pair of bytes in each set
-        m_positionList[i].setFromArray(m_pFileData->GetNext(8));
+        m_positionList = new AHXPositionEntry_t[m_posListLen];
+        for (int i = 0; i < m_posListLen; i++)
+        {
+            // 8 bytes, 4 sets (for each audiochannel), pair of bytes in each set
+            m_positionList[i].setFromArray(m_pFileData->GetNext(8));
+        }
     }
     
     // track list (zero count is valid also)
     if (m_trackCount > 0)
     {
-        size_t n = m_trackCount*m_trackLen;
-        m_trackListData = new AHXTrackEntry_t[n];
-        for (int i = 0; i < n; i++)
+        m_trackListSize = m_trackCount*m_trackLen;
+        m_trackListSize += m_trackZeroSaved; // if saved or not.. this will cause problems..
+        m_trackListData = new AHXTrackEntry_t[m_trackListSize];
+        for (int i = 0; i < m_trackListSize; i++)
         {
             m_trackListData[i].setFromArray(m_pFileData->GetNext(3));
         }
@@ -113,7 +127,7 @@ bool CAhxPlayer::ParseFileInfo()
     // samples
     if (m_sampleCount > 0)
     {
-        m_sampleData = AHXSampleEntry_t[m_sampleCount];
+        m_sampleData = new AHXSampleEntry_t[m_sampleCount];
         for (int i = 0; i < m_sampleCount; i++)
         {
             m_sampleData[i].setFromArray(m_pFileData->GetNext(22));
@@ -121,11 +135,55 @@ bool CAhxPlayer::ParseFileInfo()
             // get length of playlist
             uint8_t playlistLen = m_sampleData[i].getPlaylistLen();
             
-            // TODO: playlist data..
+            // playlist data..
+            m_sampleData[i].m_playlist = new AHXPlaylistEntry_t[playlistLen];
+            for (int j = 0; j < playlistLen; j++)
+            {
+                m_sampleData[i].m_playlist[j].setFromArray(m_pFileData->GetNext(4));
+            }
         }
     }
     
-    
+    // names, filename should be first
+    size_t nPos = m_pFileData->GetCurrentPos();
+    size_t nLen = m_pFileData->GetSize();
+    if (nPos < nLen)
+    {
+        m_fileName = (char*)m_pFileData->GetAtCurrent(); // should be NULL-terminated
+        m_pFileData->SetCurrentPos(nPos + m_fileName.length());
+    }
+
+    // sample names
+    if (m_sampleCount > 0)
+    {
+        m_sampleNames = new std::string[m_sampleCount];
+        int i = 0;
+        while ((nLen - nPos) > 1)
+        {
+            nPos = m_pFileData->GetCurrentPos();
+            
+            m_sampleNames[i] = (char*)m_pFileData->GetAtCurrent();
+            nPos += m_sampleNames[i].length();
+            
+            m_pFileData->SetCurrentPos(nPos);
+            i++;
+        }
+    }
     return true;
 }
 
+DecodeCtx *CAhxPlayer::PrepareDecoder()
+{
+    // use default implementation for now..
+    m_pDecodeCtx = new DecodeCtx();
+    
+    
+    return m_pDecodeCtx;
+}
+
+size_t CAhxPlayer::DecodePlay(void *pBuffer, const size_t nBufSize)
+{
+    // 
+
+    return 0;    
+}
--- AhxPlayer.h
+++ AhxPlayer.h
@@ -6,6 +6,10 @@
 //
 // Based on documentation by: Stuart Caie
 //
+// Note on implementation:
+// could use bitfield-structs in some data
+// but struct packing might be a problem with some compilers
+// so I'd rather not do that and done in harder way..
 //
 // Author: Ilkka Prusi, 2011
 // Contact: ilkka.prusi@gmail.com
@@ -68,7 +72,6 @@ struct AHXPositionEntry_t
     }
 };
 
-
 // TRL-count of these on each track..
 struct AHXTrackEntry_t
 {
@@ -96,8 +99,8 @@ struct AHXTrackEntry_t
         
         // remaining two bits of first byte and
         // upper four bits from second byte
-        m_sample = (((data[0] & 0x3) << 6) 
-                | ((data[1] & 0xFC) >> 2);
+        m_sample = ( ((data[0] & 0x3) << 4) 
+                | ((data[1] & 0xFC) >> 2) );
         
         m_command = (data[1] & 0x0F); // lower 4 bits
         m_commandData = data[2]; // third byte as-is
@@ -107,24 +110,67 @@ struct AHXTrackEntry_t
 // single entry in playlist
 struct AHXPlaylistEntry_t
 {
+    uint8_t m_fx2Command;
+    uint8_t m_fx1Command;
+    uint8_t m_waveForm;
+    uint8_t m_fixNote;
+    uint8_t m_noteData;
+    uint8_t m_fx2Data;
+    uint8_t m_fx1Data;
     
     // constructor
     AHXPlaylistEntry_t()
     {
+        m_fx2Command = 0;
+        m_fx1Command = 0;
+        m_waveForm = 0;
+        m_fixNote = 0;
+        m_noteData = 0;
+        m_fx2Data = 0;
+        m_fx1Data = 0;
+    }
+    
+    // values from four bytes
+    void setFromArray(uint8_t *data)
+    {
+        // first three bits
+        m_fx2Command = ((data[0] & 0xE0) >> 5); // first 3 bits
+        m_fx1Command = ((data[0] & 0x1C) >> 2); // second 3 bits
+        
+        // remaining two bits of first byte and
+        // uppermost bit from second byte
+        m_waveForm = ( ((data[0] & 0x3) << 2)
+                | ((data[1] & 0x80) >> 7) );
+
+        // single bit
+        m_fixNote = ((data[1] & 0x40) ? 1 : 0);
+        m_noteData = (data[1] & 0x3F); // 6 bits
+        
+        m_fx2Data = data[2]; // third byte as-is
+        m_fx1Data = data[3]; // fourth byte as-is
     }
 };
-
 
 // data of single sample
 struct AHXSampleEntry_t
 {
     // all values in sample..
+    // TODO: write "open" or add accessors?
     uint8_t m_data[22];
+    
+    // playlist of sample, PLEN count
+    // per-sample, right ?
+    AHXPlaylistEntry_t *m_playlist;
     
     // constructor
     AHXSampleEntry_t()
     {
         ::memset(m_data, 0, 22);
+    }
+    // destructor
+    ~AHXSampleEntry_t()
+    {
+        delete [] m_playlist;
     }
     
     void setFromArray(uint8_t *data)
@@ -134,7 +180,12 @@ struct AHXSampleEntry_t
     
     uint8_t getPlaylistLen()
     {
+        // PLEN
         return m_data[21];
+    }
+    uint8_t getPlaylistDefaultSpeed()
+    {
+        return m_data[20];
     }
 };
 
@@ -152,7 +203,11 @@ protected:
     };
     AhxFormat m_enAhxFormat;
     
-    uint8_t m_trackSave;
+    // if track zero is saved in file:
+    // might not be if it is empty (use m_trackZero then),
+    // otherwise read trackcount +1
+    uint8_t m_trackZeroSaved;
+    
     uint8_t m_playSpeed; // SPD (frequency multiplier for speed)
     uint16_t m_posListLen; // LEN, 1..999
     uint16_t m_restartPoint; // RES 0..(LEN-1)
@@ -171,6 +226,9 @@ protected:
     // see m_posListLen
     AHXPositionEntry_t *m_positionList;
 
+    // whole size of track data..
+    size_t m_trackListSize;
+
     // see m_trackCount and m_trackLen
     // also check m_trackSave (if TRK 0 was saved in file)
     AHXTrackEntry_t *m_trackListData;
@@ -182,12 +240,25 @@ protected:
     // see m_sampleCount
     AHXSampleEntry_t *m_sampleData;
     
+    // filename from metadata in file
+    std::string m_fileName;
+    
+    // sample names (m_sampleCount)
+    std::string *m_sampleNames;
     
 public:
     CAhxPlayer(CReadBuffer *pFileData);
     virtual ~CAhxPlayer();
     
     virtual bool ParseFileInfo();
+    
+    // TODO: check parameters later..
+    // prepare for playback
+    virtual DecodeCtx *PrepareDecoder();
+    
+    // something like this to decode into buffer
+    // for device-independent output in playback
+    virtual size_t DecodePlay(void *pBuffer, const size_t nBufSize);
 };
 
 #endif // AHXPLAYER_H
